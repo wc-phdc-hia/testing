fc0eff297df66ea14b11d6b9a0e87605be98f7b2
Tsholo Kumalo <tsholofelo.kumalo@westerncape.gov.za>
2023-07-31 07:55:05 +0200

    tests playwright smoke tests

--- playwright.config.js
+++ playwright.config.js
@@ -65,23 +65,23 @@ const config = {
        dependencies: ['setup'],
     },
 
-    // {
-    //   name: "firefox",
-    //   use: {
-    //     ...devices["Desktop Firefox"],
-    //     storageState: 'playwright/.auth/user.json',
-    //   },
-    //    dependencies: ['setup'],
-    // },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        storageState: 'playwright/.auth/user.json',
+      },
+       dependencies: ['setup'],
+    },
 
-    // {
-    //   name: "webkit",
-    //   use: {
-    //     ...devices["Desktop Safari"],
-    //     storageState: 'playwright/.auth/user.json',
-    //   },
-    //    dependencies: ['setup'],
-    // },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+        storageState: 'playwright/.auth/user.json',
+      },
+       dependencies: ['setup'],
+    },
 
     /* Test against mobile viewports. */
     // {

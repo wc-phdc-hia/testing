ff97a850577aae05e5fad9f295436a0bbf314969
tsholo <kumalotsholo@gmail.com>
2024-11-12 13:49:11 +0200

    Three tests modified

---
+++ tests/ChangePassword.test.mjs
@@ -0,0 +1,23 @@
+import { test } from "@playwright/test";
+import { ChangePwdPage } from "../playwright/pages/ChangePwdPage.mjs";
+
+let page;
+let changePwdPage;
+
+test.beforeAll(async ({ browser }) => {
+	page = await browser.newPage();
+	changePwdPage = new ChangePwdPage(page);
+});
+
+test.afterAll(async () => {
+	await page.close();
+});
+
+test("ChangePassword test", async ({}, testInfo) => {
+	await changePwdPage.ChangePwd();
+	const screenshot = await page.screenshot();
+	await testInfo.attach("ChangePassword test screenshot", {
+		body: screenshot,
+		contentType: "image/png",
+	});
+});

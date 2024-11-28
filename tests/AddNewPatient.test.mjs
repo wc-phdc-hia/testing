f206b7f204a4b059ce8d33b0fd343094ac7cc097
tsholo <kumalotsholo@gmail.com>
2024-11-28 15:43:54 +0200

    Created AddNewPatient test function

--- tests/AddNewPatient.test.mjs
+++ tests/AddNewPatient.test.mjs
@@ -1,22 +1,8 @@
 import { test } from "@playwright/test";
-// eslint-disable-next-line import/no-named-as-default
-import AddNewPatientPage from "../playwright/pages/AddNewPatientPage.mjs";
+import AddNewPatient from "../playwright/Functions/AddNewPatient.mjs";
 
-let page;
-let addNewPatientPage;
-
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-  addNewPatientPage = new AddNewPatientPage(page);
-});
-
-test.afterAll(async () => {
-  await page.close();
-});
-
-// eslint-disable-next-line no-empty-pattern
-test("AddNewPatient test", async ({}, testInfo) => {
-  await addNewPatientPage.AddPatient();
+test("Add a new patient", async ({ page }, testInfo) => {
+  await AddNewPatient(page);
   const screenshot = await page.screenshot();
   await testInfo.attach("AddNewPatient test screenshot", {
     body: screenshot,
